3b9856346f6cc3e69e2207b9e20f76d14760a195
Pasindu Priyashan <adpasindupriyashan@icloud.com>
2026-05-19 19:10:49 +0530

    chore: configure paginator to use Bootstrap 5 in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers {
 
+    use Illuminate\Pagination\Paginator;
     use Illuminate\Support\Facades\Blade;
     use Illuminate\Support\ServiceProvider;
 
@@ -27,6 +28,8 @@ namespace App\Providers {
             Blade::directive('currencySymbol', function () {
                 return "<?php echo auth()->check() ? auth()->user()->currency ?? '$' : '$'; ?>";
             });
+
+            Paginator::useBootstrapFive();
         }
     }
 }
